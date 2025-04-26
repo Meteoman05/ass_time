c827f060c2c084bdcaea3e28cae2f03d5485947f
Damir <damir00001234@mail.ru>
2025-04-26 23:48:25 +0300

    Got array of final string!

--- a.asm
+++ a.asm
@@ -1,14 +1,14 @@
 include console.inc
 
 .data
-	stop dd '@%#%'
+	stop dd '%#%@'
 	slst struc
 		s db 0
 		next dd 0
 	slst ends
 	len dd 0
 	lst dd ?
-	
+	arr dd ?
 	
 	
 
@@ -79,29 +79,198 @@ outlst proc ;(link_lst)
 	pop ebp
 	ret 4
 outlst endp
+
+readi proc ;(buffer1, buffer2) -> edx:eax
+	push ebp
+	mov ebp, esp
+	push ebx
+	
+	mov eax, [ebp+8]
+	mov edx, [ebp+12]
+	
+	shld edx, eax, 8
+	and edx, 0FFFFh
+	shl eax, 8
+	inchar al
+	
+	cmp dl, '@'
+	je sus
+	jmp @f
+	sus:
+	cmp eax, stop
+	je amogus
+	jmp @f
+	amogus:
+	cmp dh, '\'
+	je psi
+	xor eax, eax
+	mov al, dh
+	mov edx, 080000000h
+	jmp fin
+	
+	psi:
+	mov edx, 07FFFFFFFh
+	jmp fin
+	
+	@@:
+	cmp ax, '\\'
+	jne fin
+	mov al, 0
+	
+	fin:
+	xor ebx, ebx
+	shld ebx, eax, 16
+
+	pop ebx
+	mov esp, ebp
+	pop ebp
+	ret 2*4
+readi endp
+
+input proc ; (link_lst, link_len_lst)
+	push ebp
+	mov ebp, esp
+	
+	push ebx
+	push esi
+	
+	mov eax, 0
+	mov ebx, [ebp+8]
+	mov ecx, 0
+	mov edx, 0
+	
+	inp:
+		push edx
+		push eax
+		call readi
+		
+		cmp edx, 080000000h
+		je final
+		
+		cmp edx, 07FFFFFFFh
+		je psfin
+		
+		cmp dh, 0
+		jz cont
+		
+		movzx esi, dh
+		push eax
+		push edx
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+		pop edx
+		pop eax
+		inc ecx
+		jmp cont
+		
+		psfin:
+		mov esi, '@'
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+		mov esi, '%'
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+		mov esi, '#'
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+		mov esi, '%'
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+		mov esi, '@'
+		push esi
+		push [ebp+12]
+		push [ebp+8]
+		call append
+
+		
+		mov eax, 0
+		mov edx, 0
+		
+		cont:
+		jmp inp
+	
+	final:
+	cmp al, 0
+	jz @f
+	
+	movzx esi, al
+	push esi
+	push [ebp+12]
+	push [ebp+8]
+	call append
+	
+	@@:
+	pop esi
+	pop ebx
+	
+	mov esp, ebp
+	pop ebp
+	ret 2*4
+input endp
+
+lst_to_arr proc ;(link_lst, len_lst) -> eax
+	push ebp
+	mov ebp, esp
+	
+	push ebx
+	push esi
+	
+	mov esi, 0
+	mov ebx, [ebp+8]
+	mov ecx, [ebp+12]
+	inc ecx
+	new ecx
+	mov ecx, 0
+	
+	
+	assume ebx:ptr slst
+	@@:
+		mov dl, [ebx].s
+		mov [eax+ecx], dl
+		mov esi, [ebx].next
+		dispose ebx
+		mov ebx, esi
+		inc ecx
+		cmp [ebx].next, 0
+		jnz @b
+	dispose ebx
+	assume ebx:nothing
+	pop esi
+	pop ebx
+	
+	mov esp, ebp
+	pop ebp
+	ret 2*4
+lst_to_arr endp
 start:
 push offset len
 call init
 mov lst, eax
 
-push 'O'
-push offset len
-push lst
-call append
-outstrln 'OKEY'
 
-push 'k'
 push offset len
 push lst
-call append
-
-push '.'
-push offset len
-push lst
-call append
+call input
 
 push lst
 call outlst
+newline
+
+push len
+push lst
+call lst_to_arr
+
+outstr eax
 newline
 exit 0
 end start
